0f2678e234f174b7c834738a6d218152bc97ac7b
tepiur <st.iurin@mail.ru>
2023-11-17 01:10:12 +0500

    Initial Commit

---
+++ Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sliders
+//
+//  Created by Stefan Iurin on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
