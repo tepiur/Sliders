23d1da87d40f0b24fd74333638b3e7910ca67283
tepiur <st.iurin@mail.ru>
2023-11-19 01:38:59 +0500

    arrangement of interface elements, application logic

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    //MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupSlider(redSlider, .red, 0.40)
+        setupSlider(greenSlider, .green, 0.70)
+        setupSlider(blueSlider, .blue, 0.30)
+        
+        setupLabel(redValueLabel, redSlider)
+        setupLabel(greenValueLabel, greenSlider)
+        setupLabel(blueValueLabel, blueSlider)
+        
+        setupView()
     }
 
-
+    //MARK: - vIBAction
+    
+    @IBAction func redSliderAction() {
+        setupLabel(redValueLabel, redSlider)
+    }
+    @IBAction func greenSliderAction() {
+        setupLabel(greenValueLabel, greenSlider)
+    }
+    @IBAction func blueSliderAction() {
+        setupLabel(blueValueLabel, blueSlider)
+    }
+    
+    //MARK: - private functions
+    
+    private func setupSlider(_ slider: UISlider, _ color: UIColor, _ value: Float) {
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = color
+        slider.value = value
+    }
+    
+    private func setupLabel(_ label: UILabel, _ slider: UISlider) {
+        label.text = String(format: "%.2f", slider.value)
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                           green: CGFloat(greenSlider.value),
+                                           blue: CGFloat(blueSlider.value),
+                                           alpha: 1)
+    }
 }
 
